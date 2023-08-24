94d1c9c966a922a3654149450cf9b80f49b4970f
Fernando dos Santos <nando4803@gmail.com>
2023-08-24 20:50:10 +0100

    feat: connect User to database

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { UserController } from '@/user/user.controller';
 import { UserService } from '@/user/user.service';
+import { UserEntity } from '@/user/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
   providers: [UserService],
 })
