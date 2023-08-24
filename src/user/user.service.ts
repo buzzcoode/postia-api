f8c772d8cb644754757b4aebe751c43f8904259d
Fernando dos Santos <nando4803@gmail.com>
2023-08-24 21:25:25 +0100

    feat: add token logic

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
 
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserEntity } from '@/user/user.entity';
+import { JWT_SECRET_KEY } from '@/config';
+import { UserResponseInterface } from './types/userResponse.interface';
 
 @Injectable()
 export class UserService {
@@ -16,5 +19,25 @@ export class UserService {
     Object.assign(newUser, createUserDto);
     console.log('newUser', newUser);
     return await this.userRepository.save(newUser);
+  }
+
+  generateJwt(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        usename: user.username,
+        email: user.email,
+      },
+      JWT_SECRET_KEY,
+    );
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user),
+      },
+    };
   }
 }
