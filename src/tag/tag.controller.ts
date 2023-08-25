413215bb5f19933e3485bd8b4b589bcb8576c3f6
Fernando dos Santos <nando4803@gmail.com>
2023-08-25 11:14:40 +0100

    feat: add prefix /api/v1

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 
 import { TagService } from '@/tag/tag.service';
 
-@Controller('tags')
+@Controller('/api/v1/')
 export class TagController {
   constructor(private readonly tagsService: TagService) {}
 
-  @Get()
+  @Get('tags')
   async findAll(): Promise<{ tags: string[] }> {
     const tags = await this.tagsService.findAll();
     return {
