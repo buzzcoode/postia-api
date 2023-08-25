f043b907c09b26c402d7be58a69267094857eeff
Fernando dos Santos <nando4803@gmail.com>
2023-08-25 01:40:04 +0100

    feat: add auth guards

---
+++ src/user/guards/auth.guard.ts
@@ -0,0 +1,20 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+
+import { ExpressRequest } from '@/types/expressRequest.interface';
+
+export class AuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest<ExpressRequest>();
+
+    if (request.user) {
+      return true;
+    }
+
+    throw new HttpException('Not authorized', HttpStatus.UNAUTHORIZED);
+  }
+}
